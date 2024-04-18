b7d3bb1e7f0fc2d9b725dbbed9f08bfb4226fcf1
GimmiPrud <prudenzig@gmail.com>
2024-04-18 12:50:52 +0200

    Risolto secondo esercizio(2-4)

--- lezione1.py
+++ lezione1.py
@@ -1,12 +1,38 @@
 # Giammarco Prudenzi                       18/04/2024
 
-# ES. 2-3
-'''Personal Message: Use a variable to represent a person’s name, and print a message to that person.
+# ES. 2-3:
+'''Use a variable to represent a person’s name, and print a message to that person.
 Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”'''
 
-# soluzione ES. 2-3
+# Soluzione ES. 2-3:
 name: str = "Alessio"
 
 print(f"Ciao {name} would you like to learn some python today")
+
+# ES. 2-4:
+'''Use a variable to represent a person’s name. 
+then print that person’s name in lowercase, uppercase, and title case.'''
+
+# Soluzione ES. 2-4:
+N: str = "Carletto"
+
+print(N.upper(), N.title(), N.lower())
+# oppure:
+
+N: str = "Carletto"
+
+N_lower = N.lower()
+N_upper = N.upper()
+N_title = N.title()
+
+print(N_lower)
+print(N_title)
+print(N_upper)
+
+  
+
+
+
+
 
 

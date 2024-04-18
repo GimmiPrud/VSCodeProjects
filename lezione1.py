50dbcecdfc5753203927d51b1223d68181e788bf
GimmiPrud <prudenzig@gmail.com>
2024-04-18 12:25:18 +0200

    Risolto primo esercizio(2-3)

---
+++ lezione1.py
@@ -0,0 +1,12 @@
+# Giammarco Prudenzi                       18/04/2024
+
+# ES. 2-3
+'''Personal Message: Use a variable to represent a person’s name, and print a message to that person.
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”'''
+
+# soluzione ES. 2-3
+name: str = "Alessio"
+
+print(f"Ciao {name} would you like to learn some python today")
+
+

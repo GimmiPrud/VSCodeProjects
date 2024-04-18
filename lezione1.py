11fe8ccb8534f7253a5d1cfcd74f02bb0ad2cb59
GimmiPrud <prudenzig@gmail.com>
2024-04-18 13:45:38 +0200

    Risolto quinto esercizio(3-1)

--- lezione1.py
+++ lezione1.py
@@ -51,12 +51,12 @@ File_name: str = 'python_notes.txt'
 
 print(File_name.removesuffix('.txt'))
 
-# ES. 3.1:
+# ES. 3-1:
 
 '''Store the names of a few of your friends in a list called names. 
 Print each person’s name by accessing each element in the list, one at a time.'''
 
-# Soluzione ES. 3.1:
+# Soluzione ES. 3-1:
 
 names: list = ["Michele","Mattia","Giacomo","Andrea","Federico"]
 

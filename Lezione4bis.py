c81c346c6726e59ffcd5ec5cc0b5436b9c2ca740
GimmiPrud <prudenzig@gmail.com>
2024-04-22 17:45:37 +0200

    convertitore numeri-stringhe

---
+++ Lezione4bis.py
@@ -0,0 +1,19 @@
+
+def convert_to_title(col_number: int) -> str:
+
+    result: str = ""
+    while col_number > 0:
+
+        resto: int = (col_number-1) % 26 # resto
+
+        result = chr(resto + ord('A')) + result
+
+        col_number=(col_number-1) // 26
+
+    return result
+
+
+
+
+
+

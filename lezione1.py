56a4c832f1f49191a4b29d585e72167cc71f96af
GimmiPrud <prudenzig@gmail.com>
2024-04-22 16:43:26 +0200

    svolgendo ES. 3-3

--- lezione1.py
+++ lezione1.py
@@ -82,6 +82,18 @@ namess: list = ["Michele","Mattia","Giacomo","Andrea","Federico"]
 for name in namess:
     print(f"{name} cosa fai oggi pomeriggio ?")
 
+# Es. 3-3:
+
+'''Think of your favorite mode of transportation,
+such as a motorcycle or a car, and make a list that stores several examples.
+Use your list to print a series of statements about these items, 
+such as “I would like to own a Honda motorcycle.”'''
+
+# Soluzione ES. 3-3
+
+machine: list = ["bmv","audi","toyota"]
+
+
 
 
 

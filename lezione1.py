7e1a1bbb1ba4b438d89a1829718bc34fd00c870d
GimmiPrud <prudenzig@gmail.com>
2024-04-23 17:32:10 +0200

    finiti ES. 3-4, 3-5, 3-6, 3-7

--- lezione1.py
+++ lezione1.py
@@ -99,55 +99,7 @@ print(f"vorrei tanto possedere un {machine[1]} R1")
 
 print(f"spero un giorno di correre con una {machine[2]} Supra")
 
-# ES. 3-4:
-
-'''If you could invite anyone, living or deceased, to dinner, who would you invite?
-Make a list that includes at least three people you’d like to invite to dinner.
-Then use your list to print a message to each person, inviting them to dinner'''
-
-# Soluzione ES. 3-4:
-
-people: list = ["Giulio Cesare","Napoleone","Dio"]
-
-for p in people:
-    
-    print(f" Ciao {p} ci saresti questo fine settimana per una cena, ho già sentito gli altri")
-
-# ES. 3-5:
-
-'''You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
-• Start with your program from Exercise 3-4.
-Add a print() call at the end of your program, stating the name of the guest who can’t make it.
-• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
-• Print a second set of invitation messages, one for each person who is still in your list.'''
-
-# Soluzione ES. 3-5:
-
-people: list = ["Giulio Cesare","Napoleone","Dio"]
-
-for p in people:
-    
-    print(f" Ciao {p} ci saresti questo fine settimana per una cena, ho già sentito gli altri")
-    
-print(f" Niente ,{people[2]} come sempre non c'è ")
-
-people[2] = "Maradona"
-
-for p in people:
-    print(f" Ciao {p}, cena ore 20:00, puntuale")
-    
-# ES. 3-6:
-'''
-You just found a bigger dinner table, so now more space is available.
-Think of three more guests to invite to dinner.
-• Start with your program from Exercise 3-4 or 3-5.
-Add a print() call to the end of your program, informing people that you found a bigger table.
-• Use insert() to add one new guest to the beginning of your list.
-• Use insert() to add one new guest to the middle of your list.
-• Use append() to add one new guest to the end of your list.
-• Print a new set of invitation messages, one for each person in your list.'''
-
-# Soluzione ES. 3-6:
+# Soluzione ES. 3-4, 3-5, 3-6, 3,7:
 
 people: list = ["Giulio Cesare","Napoleone","Dio"]
 
@@ -172,19 +124,47 @@ people.append("Bruce Lee")
 
 for p in people:
     print(f" Buonasera {p} alla fine siamo sei, ci vediamo direttamente a ristorante")
-    
-# ES. 3-7:
-'''
-You just found out that your new dinner table won’t arrive in time for the dinner, and now you have space for only two guests.
-• Start with your program from Exercise 3-6.
-Add a new line that prints a message saying that you can invite only two people for dinner.
-• Use pop() to remove guests from your list one at a time until only two names remain in your list.
-Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
-• Print a message to each of the two people still on your list, letting them know they’re still invited.
-• Use del to remove the last two names from your list, so you have an empty list.
-Print your list to make sure you actually have an empty list at the end of your program.'''
 
-# Soluzione ES. 3-7
+print(" Ragazzi mi dispiace ma non abbaimo piu il tavolo, c'è solo un tavolo per tre")
+
+print(f"mi dispiace {people[0]} ma non posso più invitarti a cena")
+
+people.pop(0)
+
+print(f"mi dispiace {people[1]} ma non posso più invitarti a cena")
+
+people.pop(1)
+
+print(f"mi dispiace {people[2]} ma non posso più invitarti a cena")
+
+people.pop(2)
+
+print(f"mi dispiace {people[2]} ma non posso più invitarti a cena")
+
+people.pop(1)
+
+for p in people:
+    print(f"{p} tu sei invitato a cena")
+
+del(people[0:])
+
+print(people)
+
+# exit() per uscire dadl programma.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
